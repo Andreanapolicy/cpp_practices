d6cbfb7a96148ecd35ea05c508f96bf015c7247b
Andreanapolicy <daa.and@mail.ru>
2023-05-26 20:48:13 +0300

    LFU cache. Работаю над самой простой версией. Реализовал базовое получение элемента.

--- cpp_practices/lfu_cache/lfu_cache/LFUCache.h
+++ cpp_practices/lfu_cache/lfu_cache/LFUCache.h
@@ -1,20 +1,56 @@
 #pragma once
+#include <list>
+#include <unordered_map>
+#include <optional>
 
 namespace lfu_cache
 {
 
-template <class Element, class Key = int>
+template <class ElementT, class KeyT = int>
 class LFUCache
 {
 public:
-	LFUCache(size_t size);
+	explicit LFUCache(size_t size)
+		: m_size(size)
+	{
+	}
 
-	size_t GetCacheSize(Key key) const;
+	bool IsFull() const
+	{
+		return m_cache.size() == m_size;
+	}
 
-	void SetElement(Key key, const Element& element);
+	void SetElement(KeyT key, const ElementT& element)
+	{
+		
+	}
+
+	std::optional<ElementT> GetElement(KeyT key) const
+	{
+		auto it = std::find_if(m_cache.begin(), m_cache.end(), [key](const CacheElement& element) {
+			return element.key == key;
+		});
+
+		if (it == m_cache.end())
+		{
+			return std::nullopt_t;
+		}
+
+		it->priority++;
+		return it->element;
+	}
 
 private:
+	struct CacheElement
+	{
+		KeyT key;
+		ElementT element;
+		int priority;
+	};
+
 	size_t m_size;
+
+	std::list<CacheElement> m_cache;
 };
 
 } // namespace lfu_cache
--- cpp_practices/lfu_cache/lfu_cache/main.cpp
+++ cpp_practices/lfu_cache/lfu_cache/main.cpp
@@ -1,6 +1,12 @@
 #include <vector>
+#include <iostream>
+#include "LFUCache.h"
 
 int main()
 {
-	LFUCache lfuCache();
+	size_t size = 1;
+	lfu_cache::LFUCache<int, int> cache{size};
+
+	std::cout << cache.IsFull() << std::endl;
+	cache.SetElement(1, 2);
 }
